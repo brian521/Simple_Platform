9416422799987701013231b96df651afb8e202b3
brian521 <brian52125@gmail.com>
2026-01-12 10:44:48 +0900

    Init

---
+++ Task6/Intermediate/Build/Win64/x64/Task6Editor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/user/Desktop/NBC/Simple_Platform/Task6/Intermediate/Build/Win64/x64/Task6Editor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ Task6/Intermediate/Build/Win64/x64/Task6Editor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/user/Desktop/NBC/Simple_Platform/Task6/Intermediate/Build/Win64/x64/Task6Editor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
